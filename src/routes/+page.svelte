e17765d4b1aee68355346a243ff8d34d2c329879
daniel.vladco <vladcodaniel@gmail.com>
2023-01-30 22:40:03 +0200

    feat: make initial adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
- <a class="btn" href="/import">New Project</a>
- <a class="btn" href="/#">Existing project</a>
+<div class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="/images/pandoras_box.png" alt="pandora's box" /></figure>
+    <div class="card-body">
+        <h2 class="card-title">Daniel's box</h2>
+<!--        <p>If a dog chews shoes whose shoes does he choose?</p>-->
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Open</button>
+        </div>
+    </div>
+</div>
--- src/routes/import/+page.svelte
+++
@@ -1,4 +0,0 @@
-<label class="label" for="file">
-    <span class="label-text">Excel file</span>
-</label>
-<input id="file" type="file" class="file-input file-input-bordered file-input-lg w-full max-w-xs" accept=".xlsx, .xlsm, .xlsb, .xltx"/>
